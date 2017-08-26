4a905ccafea56ea9bf823211e8ba53cec6570cca
DESKTOP-13KQ965\YOUNG <webky99@gmail.com>
2017-08-26 14:13:47 +0900

    first

---
+++ unity-clicker-game/Assets/Scenes/Scripts/DataController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour { //로직은 구현하지 말고 메터 데이터 정의
+
+
+    // Singleton class Start
+    #region [Singleton class Start]
+    static GameObject _container;
+    static GameObject Container
+    {
+        get
+        {
+            return _container;
+        }
+    }
+
+    static DataController _instance;
+    public static DataController Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _container = new GameObject();
+                _container.name = "DataController";
+                _instance = _container.AddComponent(typeof(DataController)) as DataController;
+                DontDestroyOnLoad(_container);
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public int Gold = 0;
+    public int GoldPerSec = 1;  //1초에 쌓일 골드량 정의
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ unity-clicker-game/Assets/Scenes/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text TextGold;
+	// Use this for initialization
+	void Start () {
+        TextGold.text = DataController.Instance.Gold.ToString();
+        StartCoroutine(StartCollectGold());
+	}
+	
+
+    //코루틴 - 시간에 관련된것을 실행할때
+    IEnumerator StartCollectGold()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1f);  //1초를 쉬는것
+            DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+            TextGold.text = DataController.Instance.Gold.ToString();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
